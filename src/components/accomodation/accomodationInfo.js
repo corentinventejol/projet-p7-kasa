4bca93e107e837f3228bd9af160dc1493ebd766a
corentinventejol <97588972+corentinventejol@users.noreply.github.com>
2023-10-09 09:08:11 +0200

    update
    navbar et accomodation

--- src/components/accomodation/accomodationInfo.js
+++ src/components/accomodation/accomodationInfo.js
@@ -1,62 +1,79 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import datas from '../../data/data';
-import Slider from '../../components/carousel/Carousel';
-import Collapse from '../../components/collapse/Collapse';
-import greyStar from '../../assets/grey_star.png';
-import redStar from '../../assets/red_star.png';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import datas from "../../data/data";
+import Slider from "../../components/carousel/Carousel";
+import Collapse from "../../components/collapse/Collapse";
+import greyStar from "../../assets/grey_star.png";
+import redStar from "../../assets/red_star.png";
 
 export default function AccomodationInfo() {
   let { id } = useParams();
 
-  useEffect(() => {
-  }, []);  // [] signifie que cet effet s'exécutera une seule fois après le montage initial
-  let dataCurrentAccomodation = datas.find(data => data.id === id);
+  useEffect(() => {}, []);
+  let dataCurrentAccomodation = datas.find((data) => data.id === id);
   if (!dataCurrentAccomodation) {
-	window.location='/404'
+    window.location = "/404";
   }
-  
+
   return (
     <>
-	<Slider imageSlider={dataCurrentAccomodation.pictures} />
-        <main className="accomodation">
-          <div className="accomodation_content">
-            <div className="accomodation_content_infos">
-              <h1>{dataCurrentAccomodation.title}</h1>
-              <p>{dataCurrentAccomodation.location}</p>
-              <div>
-                {dataCurrentAccomodation.tags.map((tag, index) => (
-                  <button key={index}>{tag}</button>
-                ))}
-              </div>
-            </div>
-            <div className="accomodation_content_host">
-              <div>
-                <div className="accomodation_content_host_name">
-                  <span>{dataCurrentAccomodation.host.name.split(' ')[0]}</span>
-                  <span>{dataCurrentAccomodation.host.name.split(' ')[1]}</span>
-                </div>
-                <img src={dataCurrentAccomodation.host.picture} alt="host of this accomodation" />
-              </div>
-
-              <div className="accomodation_content_host_stars">
-                {[...Array(5)].map((star, index) => {
-                  const ratingValue = index + 1;
-                  return <img key={index} src={ratingValue <= dataCurrentAccomodation.rating ? redStar : greyStar} alt="star" />;
-                })}
-              </div>
+      <Slider imageSlider={dataCurrentAccomodation.pictures} />
+      <main className="accomodation">
+        <div className="accomodation_content">
+          <div className="accomodation_content_infos">
+            <h1>{dataCurrentAccomodation.title}</h1>
+            <p>{dataCurrentAccomodation.location}</p>
+            <div>
+              {dataCurrentAccomodation.tags.map((tag, index) => (
+                <button key={index}>{tag}</button>
+              ))}
             </div>
           </div>
-          <div className="accomodation_collapse">
-            <div className="accomodation_collapse_item">
-              <Collapse title={'Description'} content={dataCurrentAccomodation.description} />
+          <div className="accomodation_content_host">
+            <div>
+              <div className="accomodation_content_host_name">
+                <span>{dataCurrentAccomodation.host.name.split(" ")[0]}</span>
+                <span>{dataCurrentAccomodation.host.name.split(" ")[1]}</span>
+              </div>
+              <img
+                src={dataCurrentAccomodation.host.picture}
+                alt="host of this accomodation"
+              />
             </div>
-            <div className="accomodation_collapse_item">
-              <Collapse title={'Équipements'} content={dataCurrentAccomodation.equipments} />
+
+            <div className="accomodation_content_host_stars">
+              {[...Array(5)].map((star, index) => {
+                const ratingValue = index + 1;
+                return (
+                  <img
+                    key={index}
+                    src={
+                      ratingValue <= dataCurrentAccomodation.rating
+                        ? redStar
+                        : greyStar
+                    }
+                    alt="star"
+                  />
+                );
+              })}
             </div>
           </div>
-        </main>
-
+        </div>
+        <div className="accomodation_collapse">
+          <div className="accomodation_collapse_item">
+            <Collapse
+              title={"Description"}
+              content={dataCurrentAccomodation.description}
+            />
+          </div>
+          <div className="accomodation_collapse_item">
+            <Collapse
+              title={"Équipements"}
+              content={dataCurrentAccomodation.equipments}
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,16 +3,18 @@ import { NavLink } from 'react-router-dom';
 import './navbar.scss';
 
 export default function Navbar() {
+
+    //activeClassName='nav_list_item_active'
     return (
         <nav className='nav'>
             <ul className='nav_list'>
                 <li className='nav_list_item'>
-                    <NavLink exact to='/' activeClassName='nav_list_item_active'>
+                    <NavLink  to='/' className={({isActive}) => isActive?"nav_list_item_active":""}  >
                         Accueil
                     </NavLink>
                 </li>
                 <li className='nav_list_item'>
-                    <NavLink to='/about' activeClassName='nav_list_item_active'>
+                    <NavLink to='/about' className={({isActive}) => isActive?"nav_list_item_active":""}>
                         A propos
                     </NavLink>
                 </li>
